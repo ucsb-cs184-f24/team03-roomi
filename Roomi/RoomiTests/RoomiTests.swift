72b20e5a17feff747e2171e729b86112f3f62c2e
alecmorrison <alecmorrison@umail.ucsb.edu>
2024-10-11 10:55:33 -0700

    am - push starter code

---
+++ Roomi/RoomiTests/RoomiTests.swift
@@ -0,0 +1,17 @@
+//
+//  RoomiTests.swift
+//  RoomiTests
+//
+//  Created by Alec Morrison on 10/11/24.
+//
+
+import Testing
+@testable import Roomi
+
+struct RoomiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

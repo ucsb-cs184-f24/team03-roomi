65a12493097269a7199155b09a04f7b2798045f0
benjaminconte <benjaminconte@ucsb.edu>
2024-10-14 16:40:57 -0700

    Blank project

---
+++ Roomi/Roomi/RoomiApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoomiApp.swift
+//  Roomi
+//
+//  Created by Benjamin Conte on 10/14/24.
+//
+
+import SwiftUI
+
+@main
+struct RoomiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

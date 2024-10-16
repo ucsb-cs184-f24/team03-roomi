9789b0704b2da7a3db8adeb25e445275d2394ea5
benjaminconte <benjaminconte@ucsb.edu>
2024-10-16 16:24:32 -0700

    LogInPage

---
+++ Roomi/Roomi/LoginInView.swift
@@ -0,0 +1,48 @@
+//
+//  LoginInView.swift
+//  Roomi
+//
+//  Created by Benjamin Conte on 10/16/24.
+//
+
+import SwiftUI
+
+struct LoginInView: View {
+    @State var email: String = ""
+    @State var password: String = ""
+    var body: some View {
+        VStack {
+            Text("Roomi")
+                .fontWeight(.heavy)
+                .font(.largeTitle)
+            
+            TextField("Email", text: $email)
+                .padding()
+                .frame(height: 55)
+                .background(Color.gray.opacity(0.2))
+                .cornerRadius(10)
+                .autocapitalization(.none)
+            
+            SecureField("Password", text: $password)
+                .padding()
+                .frame(height: 55)
+                .background(Color.gray.opacity(0.2))
+                .cornerRadius(10)
+                .autocapitalization(.none)
+            
+            Button("Login") {
+                
+            }
+            .frame(maxWidth: .infinity)
+            .frame(height: 55)
+            .background(Color.blue)
+            .foregroundStyle(.white)
+            .cornerRadius(10)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    LoginInView()
+}

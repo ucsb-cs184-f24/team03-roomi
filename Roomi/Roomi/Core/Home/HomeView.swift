ce86ab5aa1c0cee2037915dab50a89dd4a852ee8
Anderson Lee <andersonlee@ucsb.edu>
2024-10-22 23:15:20 -0700

    Implemented auth

---
+++ Roomi/Roomi/Core/Home/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  Roomi
+//
+//  Created by Anderson Lee on 10/22/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}

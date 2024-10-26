323e6a41ad13b20d38d5defd45737b891a211fca
benjaminconte <benjaminconte@ucsb.edu>
2024-10-26 14:50:39 -0700

    login view

--- Roomi/Roomi/Core/Authentication/View/LoginView.swift
+++ Roomi/Roomi/Core/Authentication/View/LoginView.swift
@@ -1,18 +1,48 @@
 //
-//  LoginView.swift
+//  LoginInView.swift
 //  Roomi
 //
-//  Created by Benjamin Conte on 10/26/24.
+//  Created by Benjamin Conte on 10/16/24.
 //
 
 import SwiftUI
 
 struct LoginView: View {
+    @State private var email: String = ""
+    @State private var password: String = ""
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack {
+                Text("Roomi")
+                    .fontWeight(.heavy)
+                    .font(.largeTitle)
+                
+                InputView(text: $email, title: "Email", placeholder: "Enter Your Email")
+                
+                InputView(text: $password, title: "Password", placeholder: "Enter Your password")
+                
+                ButtonView(title: "Sign In")
+                                
+                NavigationLink {
+                    SignUpView()
+                        .navigationBarBackButtonHidden(true)
+                } label: {
+                    HStack(spacing: 3) {
+                        Text("Don't have an account?")
+                        Text("Sign Up").fontWeight(.bold)
+                    }
+                    .font(.system(size: 14))
+                }
+                
+                Spacer().frame(height:20)
+                
+            }
+            .padding()
+            
+        }
     }
 }
 
 #Preview {
-    LoginView()
+    LoginInView()
 }

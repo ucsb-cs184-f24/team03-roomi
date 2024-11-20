4d171125774b58106c8778472ee5c9cc8e4455ec
benjaminconte <benjaminconte@ucsb.edu>
2024-11-20 14:34:30 -0800

    Update

--- Roomi/Roomi/Core/Profile/ProfileView.swift
+++ Roomi/Roomi/Core/Profile/ProfileView.swift
@@ -11,27 +11,52 @@ struct ProfileView: View {
     @EnvironmentObject var viewModel: AuthViewModel
     
     var body: some View {
-        NavigationStack {
-            VStack {
-                if let user = viewModel.currentUser {
-                    
-                    Text("Profile")
-                        .font(.largeTitle)
-                    
-                    Text("Email: \(user.email)")
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                    Text("Name: \(user.name)")
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                    
-                    Button("Logout") {
-                        viewModel.signOut()
+        NavigationView {
+            ZStack {
+                LinearGradient(
+                    gradient: Gradient(colors: [Color(hex: 0x4A90E2), Color(hex: 0x9013FE)]),
+                    startPoint: .top,
+                    endPoint: .bottom
+                )
+                .edgesIgnoringSafeArea(.all)
+                VStack {
+                    if let user = viewModel.currentUser {
+                        Circle()
+                            .fill(Color.gray) // Set the fill color to grey
+                            .frame(width: 150, height: 150) // Set the size of the circle
+                        
+                        Text(user.name)
+                            .font(.largeTitle).bold()
+                            .foregroundColor(.white)
+                        
+                        HStack {
+                            GenderBubble(gender: user.gender)
+                            AgeBubble(age: user.age)
+                        }
+                        
+                        Spacer()
+                        ButtonView(title: "Logout", background: .red) {
+                            viewModel.signOut()
+                        }
+                        .frame(width: 100, height: 40)
+                        
+                        ButtonView(title: "Delete Account", background: .red) {
+                            viewModel.signOut()
+                        }
+                        .frame(width: 200, height: 40)
                     }
-                    .foregroundStyle(.red)
-                    .frame(maxHeight: .infinity, alignment: .bottom)
+                }
+                .padding()
+                .frame(maxHeight: .infinity, alignment: .top)
+            }
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    NavigationLink(destination: ProfileCreationView()) {
+                        Text("Edit")
+                            .foregroundStyle(.white)
+                    }
                 }
             }
-            .padding()
-            .frame(maxHeight: .infinity, alignment: .top)
         }
     }
 }

2f6908a881c0204746b7c0615071af4204a6ce4e
benjaminconte <benjaminconte@ucsb.edu>
2024-11-22 10:43:16 -0800

    more + rebase

---
+++ Roomi/Roomi/Core/Profile/UpdateProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  Roomi
+//
+//  Created by Benjamin Conte on 11/22/24.
+//
+
+import SwiftUI
+
+struct UpdateProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UpdateProfileView()
+}
